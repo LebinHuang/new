30a1bd988bf006effa7ef681789a9f65226dc1e6
LebinHuang <103292721+LebinHuang@users.noreply.github.com>
2022-05-01 22:10:54 -0700

    test

--- skill.java
+++ skill.java
@@ -1,4 +1,6 @@
 public class skill{
 
-
+    public static int divison(int n, int m){
+        return n/m;
+    }
 }
---
+++ skilltest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+
+public class skilltest {
+    
+    @Test
+    public void test1(){
+        assertEquals(1, skill.divison(2, 1));
+    }
+}
